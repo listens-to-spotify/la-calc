f91013d2d647d0264ed7c184a11cb1a2a744c154
lts <kruglov.dot@yandex.ru>
2025-02-24 19:10:57 +0300

    fixed install-mac.sh

--- install-mac.sh
+++ install-mac.sh
@@ -1,7 +1,7 @@
 xcode-select --install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-chown $USER ~/.zshrc
+sudo chown $USER ~/.zshrc
 echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
 source ~/.zshrc
 brew --version
-sudo brew install qt6
+brew install qt6
