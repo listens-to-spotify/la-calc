4d4a63ec9b611b1fd5b5bbd60851240c62be96c0
lts <kruglov.dot@yandex.ru>
2025-02-24 19:38:59 +0300

    fixed build-mac.sh

--- build-mac.sh
+++ build-mac.sh
@@ -1,8 +1,8 @@
-chown -R $USER:$USER .
+chown -R $USER .
 cd app
 qmake6 la-calc.pro
 make
 make clean
-chmod a=rwx la-calc.app
+sudo chmod a=rwx la-calc.app
 rm ~/Desktop/LA-calculator
 ln la-calc.app/Contents/MacOS/la-calc ~/Desktop/LA-calculator
